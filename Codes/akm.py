89d02c474fd64188063ca775c5dca1f53857c21c
sonfack <sss.sonfack@gmail.com>
2024-09-04 23:14:25 +0200

    update object subclasses and objectDesction

--- Codes/akm.py
+++ Codes/akm.py
@@ -1,7 +1,7 @@
 from rdflib import Namespace, Graph, URIRef
-from rdflib.namespace import RDF
+from rdflib.namespace import RDF, OWL, RDFS
 
-akg_file = "teaching_akg_unaligned.ttl"
+akg_file = "../core_activity_ontology.ttl"
 g = Graph()
 g.parse(akg_file)
 akg_namespace = Namespace("http://sonfack.com/2023/12/tao/")
@@ -12,15 +12,25 @@ def read_all_activities(akg: Graph, as_str=True) -> list:
     """This function returns all activities of an activity knowledge graph
     - akg: an activity knowledge graph as parsed by RDFLib
     - as_str: (boolean) tells if the activities are simple str default = True
+
+    Example:
+    read_all_activities(g, False)
     """
-    activities_list = [str(activity) if as_str else activity for activity in akg.subjects(predicate=RDF.type,                                                                                          object=cao_namespace.Activity, unique=True)]
+    activities_list = [str(activity) if as_str else activity for activity in akg.subjects(predicate=RDF.type,                                                  object=cao_namespace.Activity, unique=True)]
     return activities_list
 
 
-def read_akg_node(node_uri: str, akg:Graph, as_str=True) -> dict:
+def read_akg_node(node_uri: str, akg: Graph, as_str=True) -> dict:
     """This function returns all elements directly linked to a akg node
     - activity_uri (string): the given activity uri in graph akg
     - akg (Graph): an activity knowledge graph as parsed by RDFLib
+    - as_str: if one want to have result as a dictionary of string.
+              if False, result is a dictionary of URIRef
+
+    Examples on how to use read_akg_node
+    read_akg_node("pypractical-f18f0361-3f12-4b3d-9459-ce2a019b4668", g, False)
+    read_akg_node("book-0626b11e-a0d8-4eab-9485-4d42a51e8581", g)
+
     """
     activity_info = {}
     activity_uri_ref = f"{akg_namespace}{node_uri}"
@@ -39,7 +49,27 @@ def read_akg_node(node_uri: str, akg:Graph, as_str=True) -> dict:
     return activity_info
 
 
-#print(read_all_activities(g, False))
+def ontology_extraction(akg: Graph) -> None:
+    onto_g = Graph()
+    owl_class = f"<{OWL.Class}>"
+    rdf_type = f"<{RDF.type}>"
+    rdfs_subclass = f"<{RDFS.subClassOf}>"
+    print(owl_class)
+    q_1 = """CONSTRUCT
+            WHERE { ?class """+rdf_type+""" """+owl_class+""".
+                          ?class ?predicate ?object .
+            }"""
+    q_2 = """CONSTRUCT
+            WHERE { ?class """+rdf_type+""" """+owl_class+""".
+                    ?subclass """+rdfs_subclass+""" ?class.
+                    ?subclass ?subpred ?subobj .   
+                }"""
+    resp_1 = g.query(q_1)
+    print(len(resp_1))
+    resp_1.serialize(destination="g_1.ttl", format="turtle")
+    resp_2 = g.query(q_2)
+    print(len(resp_2))
+    resp_2.serialize(destination="g_2.ttl", format="turtle")
 
-#print(read_akg_node("pypractical-f18f0361-3f12-4b3d-9459-ce2a019b4668", g, False))
-#print(read_akg_node("book-0626b11e-a0d8-4eab-9485-4d42a51e8581", g))
+    
+ontology_extraction(g)
